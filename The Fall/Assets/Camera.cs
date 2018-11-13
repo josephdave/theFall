050f5ed65f02e6ad08656e25b8e364e111e11ea7
josephdave <38258284+josephdave@users.noreply.github.com>
2018-11-13 17:21:26 -0500

    Version Base
    - Camara
    - Edificio
    - Bolita

---
+++ The Fall/Assets/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    void Start () {
+        offset = transform.position - player.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x,player.transform.position.y,transform.position.z);
+
+    }
+}
